980dc4d87c7e6f2a14ab6ff7b2225ac427d0c1c4
Huan <39738858+huanth06@users.noreply.github.com>
2024-06-07 15:11:52 +0700

    Xây dựng component hiển thị thông tin của danh sách sinh viên

---
+++ react-components/student-info/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentInfoComponent from './components/StudentInfoComponent';
+
+function App() {
+  return (
+    <>
+      <StudentInfoComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ react-components/student-info/src/components/StudentInfoComponent.js
@@ -0,0 +1,51 @@
+import React from 'react'
+const students = [
+    {
+        id: 1,
+        name: 'A',
+        age: 18,
+        address: 'Ha Noi'
+    },
+    {
+        id: 2,
+        name: 'B',
+        age: 19,
+        address: 'Hue'
+    },
+    {
+        id: 3,
+        name: 'C',
+        age: 20,
+        address: 'Da Nang'
+    },
+    {
+        id: 4,
+        name: 'C',
+        age: 21,
+        address: 'TPHCM'
+    },
+]
+
+export default function StudentInfoComponent() {
+    return (
+        <div>
+            <table>
+                <caption>Students</caption>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Address</th>
+                </tr>
+                {students.map(student => (
+                    <tr>
+                        <td>{student.id}</td>
+                        <td>{student.name}</td>
+                        <td>{student.age}</td>
+                        <td>{student.address}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+}
